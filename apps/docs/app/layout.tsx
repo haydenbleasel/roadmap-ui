051241ea1074b80332bd837157d0d8ceca30b60f
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-09 14:29:44 -0400

    Fix GitHub icon

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -5,27 +5,31 @@ import { cn } from '@repo/shadcn-ui/lib/utils';
 import { Analytics } from '@vercel/analytics/react';
 import { GeistMono } from 'geist/font/mono';
 import { GeistSans } from 'geist/font/sans';
-import type { ReactNode } from 'react';
+import type { FC, ReactNode } from 'react';
 import { ThemeProvider } from './components/theme-provider';
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <html
-      lang="en"
-      className={cn(
-        'font-sans antialiased',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-      suppressHydrationWarning
-    >
-      <body>
-        <ThemeProvider>
-          <TooltipProvider>{children}</TooltipProvider>
-          <Toaster />
-        </ThemeProvider>
-        <Analytics />
-      </body>
-    </html>
-  );
-}
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout: FC<LayoutProps> = ({ children }) => (
+  <html
+    lang="en"
+    className={cn(
+      'font-sans antialiased',
+      GeistSans.variable,
+      GeistMono.variable
+    )}
+    suppressHydrationWarning
+  >
+    <body>
+      <ThemeProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+        <Toaster />
+      </ThemeProvider>
+      <Analytics />
+    </body>
+  </html>
+);
+
+export default Layout;
