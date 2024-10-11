949fbe1a61446590f7033139a2f85be8ef8f7aa3
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-11 13:27:59 -0400

    Update gantt-context.ts

--- packages/gantt/contexts/gantt-context.ts
+++ packages/gantt/contexts/gantt-context.ts
@@ -4,7 +4,6 @@ import type { GanttContextProps } from '../types/types';
 export const GanttContext = createContext<GanttContextProps>({
   zoom: 100,
   range: 'monthly',
-  grouping: 'feature',
   columnWidth: 50,
   headerHeight: 60,
   rowHeight: 36,
