c429461a84ca6628a2aff0a8c64175438e6e3f2c
Hayden Bleasel <hello@haydenbleasel.com>
2024-10-13 19:30:10 -0400

    Build fixes

--- packages/calendar/index.tsx
+++ packages/calendar/index.tsx
@@ -2,5 +2,3 @@ export { CalendarProvider } from './components/provider';
 export { CalendarMonth } from './components/month';
 export { CalendarHeader } from './components/header';
 export { CalendarBody } from './components/body';
-
-export type { DragEndEvent } from '@dnd-kit/core';
