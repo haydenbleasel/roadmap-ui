72d1aa5382de1ba37ec66a9232595366a8576fa7
Hayden Bleasel <hello@haydenbleasel.com>
2025-02-02 11:52:31 -0500

    Add Floating and Bubble Menu

--- packages/editor/index.tsx
+++ packages/editor/index.tsx
@@ -1,13 +1,40 @@
-import { EditorProvider, type EditorProviderProps } from '@tiptap/react';
+import {
+  BubbleMenu as BubbleMenuComponent,
+  type BubbleMenuProps as BubbleMenuComponentProps,
+  EditorProvider,
+  type EditorProviderProps,
+  FloatingMenu as FloatingMenuComponent,
+  type FloatingMenuProps as FloatingMenuComponentProps,
+} from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 
-export type ProviderProps = EditorProviderProps;
+export type ProviderProps = EditorProviderProps & {
+  className?: string;
+};
 
 const defaultExtensions = [StarterKit];
 
-export const Provider = ({ extensions, ...props }: ProviderProps) => (
-  <EditorProvider
-    extensions={[...defaultExtensions, ...(extensions ?? [])]}
-    {...props}
-  />
+export const Provider = ({
+  className,
+  extensions,
+  ...props
+}: ProviderProps) => (
+  <div className={className}>
+    <EditorProvider
+      extensions={[...defaultExtensions, ...(extensions ?? [])]}
+      {...props}
+    />
+  </div>
+);
+
+export type FloatingMenuProps = Omit<FloatingMenuComponentProps, 'editor'>;
+
+export const FloatingMenu = (props: FloatingMenuProps) => (
+  <FloatingMenuComponent editor={null} {...props} />
+);
+
+export type BubbleMenuProps = Omit<BubbleMenuComponentProps, 'editor'>;
+
+export const BubbleMenu = (props: BubbleMenuProps) => (
+  <BubbleMenuComponent editor={null} {...props} />
 );
