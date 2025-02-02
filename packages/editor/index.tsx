8e967caac21beb8a5d90675c00830bfee8b6818b
Hayden Bleasel <hello@haydenbleasel.com>
2025-02-02 12:37:44 -0500

    Break apart node components

--- packages/editor/index.tsx
+++ packages/editor/index.tsx
@@ -21,7 +21,6 @@ import {
   BoldIcon,
   CheckIcon,
   CheckSquare,
-  ChevronDown,
   ChevronDownIcon,
   Code,
   CodeIcon,
@@ -82,6 +81,32 @@ export const BubbleMenu = ({ className, ...props }: BubbleMenuProps) => (
   />
 );
 
+type BubbleMenuButtonProps = {
+  name: string;
+  isActive: () => boolean;
+  command: () => void;
+  icon: LucideIcon;
+};
+
+const BubbleMenuButton = ({
+  name,
+  isActive,
+  command,
+  icon: Icon,
+}: BubbleMenuButtonProps) => (
+  <Button
+    onSelect={() => command()}
+    variant="ghost"
+    className="flex w-full gap-2"
+  >
+    <Icon size={16} className="shrink-0 text-muted-foreground" />
+    <span className="flex-1 text-left">{name}</span>
+    {isActive() ? (
+      <CheckIcon size={16} className="shrink-0 text-muted-foreground" />
+    ) : null}
+  </Button>
+);
+
 export type BubbleMenuTextButtonsProps = HTMLAttributes<HTMLDivElement>;
 
 export const BubbleMenuTextButtons = ({
@@ -130,6 +155,175 @@ export const BubbleMenuTextButtons = ({
   );
 };
 
+export const BubbleMenuNodeText = () => {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <BubbleMenuButton
+      name="Text"
+      command={() =>
+        editor.chain().focus().toggleNode('paragraph', 'paragraph').run()
+      }
+      // I feel like there has to be a more efficient way to do this – feel free to PR if you know how!
+      isActive={() =>
+        (editor &&
+          !editor.isActive('paragraph') &&
+          !editor.isActive('bulletList') &&
+          !editor.isActive('orderedList')) ??
+        false
+      }
+      icon={TextIcon}
+    />
+  );
+};
+
+export const BubbleMenuNodeHeading1 = () => {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <BubbleMenuButton
+      name="Heading 1"
+      command={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+      icon={Heading1}
+      isActive={() => editor.isActive('heading', { level: 1 }) ?? false}
+    />
+  );
+};
+
+export const BubbleMenuNodeHeading2 = () => {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <BubbleMenuButton
+      name="Heading 2"
+      command={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+      icon={Heading2}
+      isActive={() => editor.isActive('heading', { level: 2 }) ?? false}
+    />
+  );
+};
+
+export const BubbleMenuNodeHeading3 = () => {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <BubbleMenuButton
+      name="Heading 3"
+      command={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+      icon={Heading3}
+      isActive={() => editor.isActive('heading', { level: 3 }) ?? false}
+    />
+  );
+};
+
+export const BubbleMenuNodeBulletList = () => {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <BubbleMenuButton
+      name="Bullet List"
+      command={() => editor.chain().focus().toggleBulletList().run()}
+      icon={ListOrdered}
+      isActive={() => editor.isActive('bulletList') ?? false}
+    />
+  );
+};
+
+export const BubbleMenuNodeOrderedList = () => {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <BubbleMenuButton
+      name="Numbered List"
+      command={() => editor.chain().focus().toggleOrderedList().run()}
+      icon={ListOrdered}
+      isActive={() => editor.isActive('orderedList') ?? false}
+    />
+  );
+};
+
+export const BubbleMenuNodeTaskList = () => {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <BubbleMenuButton
+      name="To-do List"
+      command={() => editor.chain().focus().toggleTaskList().run()}
+      icon={CheckSquare}
+      isActive={() => editor.isActive('taskItem') ?? false}
+    />
+  );
+};
+
+export const BubbleMenuNodeQuote = () => {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <BubbleMenuButton
+      name="Quote"
+      command={() =>
+        editor
+          .chain()
+          .focus()
+          .toggleNode('paragraph', 'paragraph')
+          .toggleBlockquote()
+          .run()
+      }
+      icon={TextQuote}
+      isActive={() => editor.isActive('blockquote') ?? false}
+    />
+  );
+};
+
+export const BubbleMenuNodeCode = () => {
+  const { editor } = useCurrentEditor();
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <BubbleMenuButton
+      name="Code"
+      command={() => editor.chain().focus().toggleCodeBlock().run()}
+      icon={Code}
+      isActive={() => editor.isActive('codeBlock') ?? false}
+    />
+  );
+};
+
 export type BubbleMenuNodeSelectorProps = HTMLAttributes<HTMLDivElement> & {
   open: boolean;
   onOpenChange: (open: boolean) => void;
@@ -139,6 +333,7 @@ export const BubbleMenuNodeSelector = ({
   open,
   onOpenChange,
   className,
+  children,
   ...props
 }: BubbleMenuNodeSelectorProps) => {
   const { editor } = useCurrentEditor();
@@ -147,86 +342,12 @@ export const BubbleMenuNodeSelector = ({
     return null;
   }
 
-  const items = [
-    {
-      name: 'Text',
-      icon: TextIcon,
-      command: () =>
-        editor.chain().focus().toggleNode('paragraph', 'paragraph').run(),
-      // I feel like there has to be a more efficient way to do this – feel free to PR if you know how!
-      isActive: () =>
-        (editor &&
-          !editor.isActive('paragraph') &&
-          !editor.isActive('bulletList') &&
-          !editor.isActive('orderedList')) ??
-        false,
-    },
-    {
-      name: 'Heading 1',
-      icon: Heading1,
-      command: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
-      isActive: () => editor.isActive('heading', { level: 1 }) ?? false,
-    },
-    {
-      name: 'Heading 2',
-      icon: Heading2,
-      command: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
-      isActive: () => editor.isActive('heading', { level: 2 }) ?? false,
-    },
-    {
-      name: 'Heading 3',
-      icon: Heading3,
-      command: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
-      isActive: () => editor.isActive('heading', { level: 3 }) ?? false,
-    },
-    {
-      name: 'To-do List',
-      icon: CheckSquare,
-      command: () => editor.chain().focus().toggleTaskList().run(),
-      isActive: () => editor.isActive('taskItem') ?? false,
-    },
-    {
-      name: 'Bullet List',
-      icon: ListOrdered,
-      command: () => editor.chain().focus().toggleBulletList().run(),
-      isActive: () => editor.isActive('bulletList') ?? false,
-    },
-    {
-      name: 'Numbered List',
-      icon: ListOrdered,
-      command: () => editor.chain().focus().toggleOrderedList().run(),
-      isActive: () => editor.isActive('orderedList') ?? false,
-    },
-    {
-      name: 'Quote',
-      icon: TextQuote,
-      command: () =>
-        editor
-          ?.chain()
-          .focus()
-          .toggleNode('paragraph', 'paragraph')
-          .toggleBlockquote()
-          .run(),
-      isActive: () => editor.isActive('blockquote') ?? false,
-    },
-    {
-      name: 'Code',
-      icon: Code,
-      command: () => editor.chain().focus().toggleCodeBlock().run(),
-      isActive: () => editor.isActive('codeBlock') ?? false,
-    },
-  ];
-
-  const activeItem = items.filter((item) => item.isActive()).pop() ?? {
-    name: 'Text',
-  };
-
   return (
     <Popover modal open={open} onOpenChange={onOpenChange}>
       <PopoverTrigger asChild>
         <Button variant="ghost" className="gap-2 rounded-none border-none">
-          <span className="whitespace-nowrap text-sm">{activeItem.name}</span>
-          <ChevronDown className="h-4 w-4" />
+          <span className="whitespace-nowrap text-sm">Text</span>
+          <ChevronDownIcon size={16} />
         </Button>
       </PopoverTrigger>
       <PopoverContent
@@ -235,53 +356,11 @@ export const BubbleMenuNodeSelector = ({
         className={cn('w-48 p-1', className)}
         {...props}
       >
-        {items.map((item) => (
-          <Button
-            key={item.name}
-            onSelect={() => {
-              item.command();
-              onOpenChange(false);
-            }}
-            variant="ghost"
-            className="flex w-full gap-2"
-          >
-            <item.icon size={16} className="shrink-0 text-muted-foreground" />
-            <span className="flex-1 text-left">{item.name}</span>
-            {activeItem.name === item.name && (
-              <CheckIcon size={16} className="shrink-0 text-muted-foreground" />
-            )}
-          </Button>
-        ))}
+        {children}
       </PopoverContent>
     </Popover>
   );
 };
-
-type BubbleMenuFormatButtonProps = {
-  name: string;
-  isActive: () => boolean;
-  command: () => void;
-  icon: LucideIcon;
-};
-
-const BubbleMenuFormatButton = ({
-  name,
-  isActive,
-  command,
-  icon: Icon,
-}: BubbleMenuFormatButtonProps) => (
-  <Button
-    onSelect={() => command()}
-    variant="ghost"
-    className="flex w-full gap-2"
-  >
-    <Icon size={16} className="shrink-0 text-muted-foreground" />
-    <span className="flex-1 text-left">{name}</span>
-    {isActive() ? (
-      <CheckIcon size={16} className="shrink-0 text-muted-foreground" />
-    ) : null}
-  </Button>
-);
 
 export const BubbleMenuFormatBold = () => {
   const { editor } = useCurrentEditor();
@@ -291,7 +370,7 @@ export const BubbleMenuFormatBold = () => {
   }
 
   return (
-    <BubbleMenuFormatButton
+    <BubbleMenuButton
       name="Bold"
       isActive={() => editor.isActive('bold') ?? false}
       command={() => editor.chain().focus().toggleBold().run()}
@@ -308,7 +387,7 @@ export const BubbleMenuFormatItalic = () => {
   }
 
   return (
-    <BubbleMenuFormatButton
+    <BubbleMenuButton
       name="Italic"
       isActive={() => editor.isActive('italic') ?? false}
       command={() => editor.chain().focus().toggleItalic().run()}
@@ -325,7 +404,7 @@ export const BubbleMenuFormatStrike = () => {
   }
 
   return (
-    <BubbleMenuFormatButton
+    <BubbleMenuButton
       name="Strikethrough"
       isActive={() => editor.isActive('strike') ?? false}
       command={() => editor.chain().focus().toggleStrike().run()}
@@ -342,7 +421,7 @@ export const BubbleMenuFormatCode = () => {
   }
 
   return (
-    <BubbleMenuFormatButton
+    <BubbleMenuButton
       name="Code"
       isActive={() => editor.isActive('code') ?? false}
       command={() => editor.chain().focus().toggleCode().run()}
@@ -359,7 +438,7 @@ export const BubbleMenuFormatSubscript = () => {
   }
 
   return (
-    <BubbleMenuFormatButton
+    <BubbleMenuButton
       name="Subscript"
       isActive={() => editor.isActive('subscript') ?? false}
       command={() => editor.chain().focus().toggleSubscript().run()}
@@ -376,7 +455,7 @@ export const BubbleMenuFormatSuperscript = () => {
   }
 
   return (
-    <BubbleMenuFormatButton
+    <BubbleMenuButton
       name="Superscript"
       isActive={() => editor.isActive('superscript') ?? false}
       command={() => editor.chain().focus().toggleSuperscript().run()}
@@ -393,7 +472,7 @@ export const BubbleMenuFormatUnderline = () => {
   }
 
   return (
-    <BubbleMenuFormatButton
+    <BubbleMenuButton
       name="Underline"
       isActive={() => editor.isActive('underline') ?? false}
       command={() => editor.chain().focus().toggleUnderline().run()}
