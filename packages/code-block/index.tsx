e9f23c2cbcbb98a2f218b1b6b8cb68e83da509d9
Hayden Bleasel <hello@haydenbleasel.com>
2025-02-06 12:15:15 -0500

    Add filename-icon map

--- packages/code-block/index.tsx
+++ packages/code-block/index.tsx
@@ -1,30 +1,182 @@
 'use client';
 
+import {
+  SiAstro,
+  SiBiome,
+  SiBower,
+  SiBun,
+  SiC,
+  SiCircleci,
+  SiCoffeescript,
+  SiCplusplus,
+  SiCss,
+  SiCssmodules,
+  SiDart,
+  SiDocker,
+  SiDocusaurus,
+  SiDotenv,
+  SiEditorconfig,
+  SiEslint,
+  SiGatsby,
+  SiGitignoredotio,
+  SiGnubash,
+  SiGo,
+  SiGraphql,
+  SiGrunt,
+  SiGulp,
+  SiHandlebarsdotjs,
+  SiHtml5,
+  SiJavascript,
+  SiJest,
+  SiJson,
+  SiLess,
+  SiMarkdown,
+  SiMdx,
+  SiMintlify,
+  SiMocha,
+  SiMysql,
+  SiNextdotjs,
+  SiPerl,
+  SiPhp,
+  SiPostcss,
+  SiPrettier,
+  SiPrisma,
+  SiPug,
+  SiPython,
+  SiR,
+  SiReact,
+  SiReadme,
+  SiRedis,
+  SiRemix,
+  SiRive,
+  SiRollupdotjs,
+  SiRuby,
+  SiSanity,
+  SiSass,
+  SiScala,
+  SiSentry,
+  SiShadcnui,
+  SiStorybook,
+  SiStylelint,
+  SiSublimetext,
+  SiSvelte,
+  SiSvg,
+  SiSwift,
+  SiTailwindcss,
+  SiToml,
+  SiTypescript,
+  SiVercel,
+  SiVite,
+  SiVuedotjs,
+  SiWebassembly,
+} from '@icons-pack/react-simple-icons';
 import { type ReactNode, useEffect, useState } from 'react';
 import { codeToHtml } from 'shiki';
 
 export type CodeBlockProps = {
   children: ReactNode;
   language: string;
+  filename: string;
 };
 
-export const CodeBlock = ({ children, language }: CodeBlockProps) => {
-  return <div className="overflow-hidden rounded-md border">{children}</div>;
+const filenameIconMap = {
+  '.env': SiDotenv,
+  '*.astro': SiAstro,
+  'biome.json': SiBiome,
+  '.bowerrc': SiBower,
+  'bun.lockb': SiBun,
+  '*.c': SiC,
+  '*.cpp': SiCplusplus,
+  '.circleci/config.yml': SiCircleci,
+  '*.coffee': SiCoffeescript,
+  '*.module.css': SiCssmodules,
+  '*.css': SiCss,
+  '*.dart': SiDart,
+  Dockerfile: SiDocker,
+  'docusaurus.config.js': SiDocusaurus,
+  '.editorconfig': SiEditorconfig,
+  '.eslintrc': SiEslint,
+  'eslint.config.*': SiEslint,
+  'gatsby-config.*': SiGatsby,
+  '.gitignore': SiGitignoredotio,
+  '*.go': SiGo,
+  '*.graphql': SiGraphql,
+  '*.sh': SiGnubash,
+  'Gruntfile.*': SiGrunt,
+  'gulpfile.*': SiGulp,
+  '*.hbs': SiHandlebarsdotjs,
+  '*.html': SiHtml5,
+  '*.js': SiJavascript,
+  '*.json': SiJson,
+  '*.test.js': SiJest,
+  '*.less': SiLess,
+  '*.md': SiMarkdown,
+  '*.mdx': SiMdx,
+  'mintlify.json': SiMintlify,
+  'mocha.opts': SiMocha,
+  '*.mustache': SiHandlebarsdotjs,
+  '*.sql': SiMysql,
+  'next.config.*': SiNextdotjs,
+  '*.pl': SiPerl,
+  '*.php': SiPhp,
+  'postcss.config.*': SiPostcss,
+  'prettier.config.*': SiPrettier,
+  '*.prisma': SiPrisma,
+  '*.pug': SiPug,
+  '*.py': SiPython,
+  '*.r': SiR,
+  '*.rb': SiRuby,
+  '*.jsx': SiReact,
+  '*.tsx': SiReact,
+  'readme.md': SiReadme,
+  '*.rdb': SiRedis,
+  'remix.config.*': SiRemix,
+  '*.riv': SiRive,
+  'rollup.config.*': SiRollupdotjs,
+  'sanity.config.*': SiSanity,
+  '*.sass': SiSass,
+  '*.scss': SiSass,
+  '*.sc': SiScala,
+  '*.scala': SiScala,
+  'sentry.client.config.*': SiSentry,
+  'components.json': SiShadcnui,
+  'storybook.config.*': SiStorybook,
+  'stylelint.config.*': SiStylelint,
+  '.sublime-settings': SiSublimetext,
+  '*.svelte': SiSvelte,
+  '*.svg': SiSvg,
+  '*.swift': SiSwift,
+  'tailwind.config.*': SiTailwindcss,
+  '*.toml': SiToml,
+  '*.ts': SiTypescript,
+  'vercel.json': SiVercel,
+  'vite.config.*': SiVite,
+  '*.vue': SiVuedotjs,
+  '*.wasm': SiWebassembly,
+};
+
+export const CodeBlock = ({ children, language, filename }: CodeBlockProps) => {
+  const extension = filename ? filename.split('.').pop() : null;
+  const Icon = extension
+    ? filenameIconMap[extension as keyof typeof filenameIconMap]
+    : null;
+
+  return (
+    <div className="overflow-hidden rounded-md border">
+      {Object.values(extensionIconMap).map((Icon) => (
+        <Icon key={Icon.name} className="h-4 w-4" />
+      ))}
+      {children}
+    </div>
+  );
 };
 
 export type CodeBlockHeaderProps = {
   children: ReactNode;
-  filename: string;
 };
 
-export const CodeBlockHeader = ({
-  children,
-  filename,
-}: CodeBlockHeaderProps) => (
-  <div className="bg-secondary px-4 text-sm">
-    {filename}
-    {children}
-  </div>
+export const CodeBlockHeader = ({ children }: CodeBlockHeaderProps) => (
+  <div className="bg-secondary px-4 text-sm">{children}</div>
 );
 
 export type CodeBlockBodyProps = {
